b983bf0db29bc802a71ec26f366f2135ca569c2d
Aravindan-111 <aravindhan11195@gmail.com>
2021-12-18 18:31:38 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+let num = 5;
+let company = "cartrabbit";
+let isPublished = true;
+// WE CAN ASSIGN ANY VALUE IN "any"
+let x = "hello";
+let ids = [1, 2, 3, 4, 5];
+let arr = ["hello", 23, false];
+// Tuple
+let person = [12, "hello", true];
+// Tuple Array
+let employee;
+employee = [
+    [12, "Aravind"],
+    [13, "Muthu"],
+    [14, "Shashi"],
+    [15, "Mayu"],
+    [16, "Ragul"],
+];
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+let num: number = 5;
+let company: string = "cartrabbit";
+let isPublished: boolean = true;
+
+// WE CAN ASSIGN ANY VALUE IN "any"
+let x: any = "hello";
+
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = ["hello", 23, false];
+
+// Tuple
+let person: [number, string, boolean] = [12, "hello", true];
+// Tuple Array
+let employee: [number, string][];
+
+employee = [
+  [12, "Aravind"],
+  [13, "Muthu"],
+  [14, "Shashi"],
+  [15, "Mayu"],
+  [16, "Ragul"],
+];
