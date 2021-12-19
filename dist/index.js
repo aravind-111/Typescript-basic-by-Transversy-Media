5a34bc098b22f900c6babbe3c6b14f0171a80e38
Aravindan-111 <aravindhan11195@gmail.com>
2021-12-19 16:24:06 +0530

    updated

--- dist/index.js
+++ dist/index.js
@@ -17,3 +17,56 @@ employee = [
     [15, "Mayu"],
     [16, "Ragul"],
 ];
+// Union
+let pid = "22";
+// Enum
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 5] = "Down";
+    Direction[Direction["Left"] = 6] = "Left";
+    Direction[Direction["Rignt"] = 7] = "Rignt";
+})(Direction || (Direction = {}));
+const user = {
+    id: 1,
+    name: "aravind",
+};
+// type Assertion
+let cid = 1;
+// let customerId = <number>cid;
+let customerId = cid;
+// Functions
+function addNum(x, y) {
+    return x + y;
+}
+// Void
+function log(message) {
+    // console.log(message);
+}
+log(98);
+const user1 = {
+    id: 1,
+    name: "aravind",
+};
+const add = (x, y) => x + y;
+const sub = (x, y) => x - y;
+// Classes
+class Person {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    register() {
+        return `${this.name} is registered successfully`;
+    }
+}
+const brad = new Person(2, "Brad");
+const todd = new Person(3, "Todd");
+class Employee extends Person {
+    constructor(id, name, position) {
+        super(id, name);
+        this.position = position;
+    }
+}
+const emp = new Employee(3, "brad", "senior");
+console.log(emp.register());
--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,97 @@ employee = [
   [15, "Mayu"],
   [16, "Ragul"],
 ];
+
+// Union
+let pid: string | number = "22";
+
+// Enum
+enum Direction {
+  Up,
+  Down = 5,
+  Left,
+  Rignt,
+}
+
+// Objects
+type User = {
+  id: number;
+  name: string;
+};
+
+const user: User = {
+  id: 1,
+  name: "aravind",
+};
+
+// type Assertion
+let cid: any = 1;
+// let customerId = <number>cid;
+let customerId = cid as number;
+
+// Functions
+function addNum(x: number, y: number): number {
+  return x + y;
+}
+
+// Void
+function log(message: string | number): void {
+  // console.log(message);
+}
+log(98);
+
+// Interfaces
+interface UserInterface {
+  readonly id: number;
+  name: string;
+  age?: number;
+}
+
+const user1: UserInterface = {
+  id: 1,
+  name: "aravind",
+};
+
+interface MathFunc {
+  (x: number, y: number): number;
+}
+
+const add: MathFunc = (x: number, y: number): number => x + y;
+const sub: MathFunc = (x: number, y: number): number => x - y;
+
+interface PersonInterface {
+  id: number;
+  name: string;
+  register(): string;
+}
+
+// Classes
+class Person implements PersonInterface {
+  id: number;
+  name: string;
+
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+
+  register() {
+    return `${this.name} is registered successfully`;
+  }
+}
+
+const brad = new Person(2, "Brad");
+const todd = new Person(3, "Todd");
+
+class Employee extends Person {
+  position: string;
+
+  constructor(id: number, name: string, position: string) {
+    super(id, name);
+    this.position = position;
+  }
+}
+
+const emp = new Employee(3, "brad", "senior");
+
+console.log(emp.register());
